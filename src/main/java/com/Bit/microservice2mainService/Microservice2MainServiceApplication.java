319026ce80146cc2813ef8ce1140e8ec53c28bdf
Cem Kok <cemkok@outlook.com>
2022-07-11 19:56:47 +0300

    The project was created, application.properties was configured

---
+++ src/main/java/com/Bit/microservice2mainService/Microservice2MainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Bit.microservice2mainService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice2MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2MainServiceApplication.class, args);
+	}
+
+}
