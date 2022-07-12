1a75e86ea70bea99c745e5e1162afa55b9117e85
Cem Kok <cemkok@outlook.com>
2022-07-12 14:33:23 +0300

    business layer was created, passwordEncoder method was created under the main method to use to encode the password in business.concretes.Usermanager>addUser() 12.07.2022

---
+++ src/main/java/com/Bit/microservice2mainService/business/abstracts/UserService.java
@@ -0,0 +1,38 @@
+/**
+ * Spring Boot version 2.6.2
+ * Build Automation Tool is Maven.
+ * Java version 1.8
+ */
+package com.Bit.microservice2mainService.business.abstracts;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.Bit.microservice2mainService.entities.User;
+
+/**
+ * @author cemko
+ * @Date   12 Tem 2022
+ * @Time   13:42:22
+ * @See
+ */
+public interface UserService {
+
+	/**
+	 * @param user
+	 * @return
+	 */
+	User addUser(User user);
+
+	/**
+	 * @param username
+	 * @return
+	 */
+	Optional<User> findByUsername(String username);
+
+	/**
+	 * @return
+	 */
+	List<User> findAllUsers();
+
+}
---
+++ src/main/java/com/Bit/microservice2mainService/business/concretes/UserManager.java
@@ -0,0 +1,68 @@
+/**
+ * Spring Boot version 2.6.2
+ * Build Automation Tool is Maven.
+ * Java version 1.8
+ */
+package com.Bit.microservice2mainService.business.concretes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.Bit.microservice2mainService.business.abstracts.UserService;
+import com.Bit.microservice2mainService.dataAccess.UserDao;
+import com.Bit.microservice2mainService.entities.User;
+
+/**
+ * @author cemko
+ * @Date   12 Tem 2022
+ * @Time   13:42:07
+ * @See
+ */	
+
+@Service
+public class UserManager implements UserService{
+	
+	private UserDao userDao;
+	private PasswordEncoder passwordEncoder;
+	
+	/**
+	 * @param userDao
+	 * @param passwordEncoder
+	 */
+	@Autowired
+	public UserManager(UserDao userDao, PasswordEncoder passwordEncoder) {
+		super();
+		this.userDao = userDao;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	
+	@Override
+	public User addUser(User user) {
+		
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setCreateTime(LocalDateTime.now());
+		return  userDao.save(user);
+		
+	}
+	
+	
+	@Override
+	 public Optional<User> findByUsername(String username){
+		 
+		 return userDao.findByUsername(username);
+	 }
+	
+	
+	@Override
+	public List<User> findAllUsers(){
+		return userDao.findAll();
+	}
+	
+
+}
