d1c1380beb837d6e0cb95a78b130a4627849089b
Cem Kok <cemkok@outlook.com>
2022-07-13 17:25:56 +0300

    Gson was configured in config package

---
+++ src/main/java/com/Bit/microservice2mainService/config/GsonHttpMessageConfig.java
@@ -0,0 +1,56 @@
+/**
+ * Spring Boot version 2.6.2
+ * Build Automation Tool is Maven.
+ * Java version 1.8
+ */
+package com.Bit.microservice2mainService.config;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+
+/**
+ * @author Cem Kök
+ * @Date   13 Tem 2022
+ * @Time   17:06:19
+ * @See
+ */
+@Configuration
+public class GsonHttpMessageConfig {
+
+    @Bean
+    public GsonBuilder gsonBuilder()
+    {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class,
+                        (JsonSerializer<LocalDateTime>) (date, type, context) ->
+                        date == null ? null : new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+                .registerTypeAdapter(LocalDateTime.class,
+                        (JsonDeserializer<LocalDateTime>) (json, type, context) ->
+                        LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+    }
+
+    @Bean
+    public Gson gson(GsonBuilder gsonBuilder)
+    {
+        return gsonBuilder.create();
+    }
+
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson gson)
+    {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson);
+        return converter;
+    }
+
+}
